5a8a848b5cd4f2d352d09d232a0e42ab7eb4bda5
csboyty <csboyty@gmail.com>
2016-05-12 13:58:55 +0800

    ty

--- src/main/webapp/resources/js/src/config.js
+++ src/main/webapp/resources/js/src/config.js
@@ -10,7 +10,7 @@ var config={
     qiNiu:{
     	upTokenUrl:"getUploadKey",
         uploadDomain:"http://qiniu-plupload.qiniudn.com/",
-        bucketDomain:"http://7xsv6i.com1.z0.glb.clouddn.com/",
+        bucketDomain:"http://7xtna1.com1.z0.glb.clouddn.com/",
         swfUrl:"resources/js/lib/Moxie.swf"
     },
     ajaxUrls:{
